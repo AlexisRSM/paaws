700e91bdde69b9dbf539bfe387d9cc9837065c83
AlexisRSM <alexismendes98@hotmail.com>
2024-10-20 19:50:49 +0100

    deploy config try

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+/* I dont know what to do to deploy */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
